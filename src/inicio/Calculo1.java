8f5c433b33ad981cfb02a328c324283cbcc60abf
Curso Tarde <Curso Tarde@192.168.1.21>
2022-05-27 10:27:29 +0200

    foto1

---
+++ src/inicio/Calculo1.java
@@ -0,0 +1,26 @@
+
+public class Calculo1 {
+
+   
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in)
+ 
+        double n1, n2, s;
+        system.out.print("ingresar el numero 1?");
+        n1-sc.nextDouble();
+        system.out.print("ingresar el numero 2");
+        N2 =SC.NEXTdOUBLE[);
+        s=n1+ n2;
+       System.out.print("el resultado es : "+ s);
+
+
+
+    }
+
+}
+Algoritmo Ejemplo53
+	Escribir Sin Saltar "Ingresar el número 1? "; Leer n1
+	Escribir Sin Saltar "Ingresar el número 2? "; Leer n2
+	s <- n1 + n2
+	Escribir "El resultado es: ", s
+FinAlgoritmo
